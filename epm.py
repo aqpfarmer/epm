d3902d09ead1bc476cbf3baad879ea055aeb313d
Chris Murphy <chris@chooba.mrchips.xyz>
2019-07-20 10:06:49 -0400

    change db string to localhost on azure

--- epm.py
+++ epm.py
@@ -12,7 +12,7 @@ from sqlalchemy import or_, desc
 import math
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chris:funkytown@catfish.eastus.cloudapp.azure.com/evesde'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chris:funkytown@localhost/evesde'
 app.debug = True # disable this in production!
 app.config['SECRET_KEY'] = 'super-secret-foolish-fool112'
 app.config['SECURITY_REGISTERABLE'] = True
